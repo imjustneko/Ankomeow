3f517f4c559236e47f923faeba4e632da18a1f80
imjustneko <goodmorningamv@gmail.com>
2026-07-23 16:38:27 +0800

    feat: утсанд суулгах боломжтой (PWA) Vite орчин нэмэх
    manifest.webmanifest, service worker, лого-с гаргасан icon-ууд нэмж,
    Android/iOS-с 'Нүүр дэлгэц рүү нэмэх' хийхэд ажилладаг болгов.
    npm install && npm run dev-ээр локал сервертэй тестлэнэ.

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../tovlorokh-khamtrakh.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
