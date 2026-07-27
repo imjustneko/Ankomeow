d4dd874646bb76fb90ac858a746b2f449af2e8b8
imjustneko <goodmorningamv@gmail.com>
2026-07-27 09:59:53 +0800

    build: Tailwind-ыг CDN-ээс build-time рүү шилжүүлэв
    cdn.tailwindcss.com нь production-д тохиромжгүй бөгөөд эхний ачаалалтад
    интернэт шаарддаг байсан. Одоо tailwindcss+postcss-ээр build хийгдэж,
    13KB CSS файл болон офлайн ажиллана.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../tovlorokh-khamtrakh.jsx";
+import "./index.css";
 
 window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
