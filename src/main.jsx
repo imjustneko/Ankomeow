2b4c20d3ec486b599c3aa660c2fa590ed953de76
imjustneko <goodmorningamv@gmail.com>
2026-07-28 10:27:30 +0800

    feat: iOS хүрэлтийн глобал давхарга — bounce, tap highlight, урт дарах цэс хаав

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../tovlorokh-khamtrakh.jsx";
 import "./index.css";
+import "./ios.css";
 
 window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
